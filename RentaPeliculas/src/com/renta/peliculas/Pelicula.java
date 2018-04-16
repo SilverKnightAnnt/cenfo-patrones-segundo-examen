72f0ab52b82b76e151d0d0962b70dfabfca89b53
Ángel Marín <35677091+SilverKnightAnnt@users.noreply.github.com>
2018-04-15 18:37:12 -0600

    Arreglo gets y sets de película

--- RentaPeliculas/src/com/renta/peliculas/Pelicula.java
+++ RentaPeliculas/src/com/renta/peliculas/Pelicula.java
@@ -1,39 +1,46 @@
 package com.renta.peliculas;
 
 public class Pelicula {
-		public static final int  INFANTIL = 2;
-	    public static final int  NORMAL = 0;
-	    public static final int  ESTRENO = 1;
+	public static final int INFANTIL = 2;
+	public static final int NORMAL = 0;
+	public static final int ESTRENO = 1;
 
-	    private String nombre; 
-		private int codigoPrecio;
+	private String nombre;
+	private int codigoPrecio;
 
-		public Pelicula(String pNombre, int pCodigoPrecio) {
-			//No debería llamar al set dentro del constructor.
-			//setNombre(pNombre);
-			
-			//Por ende se sustituiría por el atributo base
-			this.nombre = pNombre;
-			this.codigoPrecio = pCodigoPrecio;
-		}
+	public Pelicula(String pNombre, int pCodigoPrecio) {
+		this.setNombre(pNombre);
+		this.setCodigoPrecio(pCodigoPrecio);
+	}
 
-		public int getCodigoPrecio() {
-			return codigoPrecio;
-		}
+	public String getNombre() {
+		return nombre;
+	}
 
-		public void persist() {
-			Registrar.add ("Movies", this);
-		}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-		public static Pelicula get(String name) {
-			return (Pelicula) Registrar.get ("Peliculas", name);
-		}
+	public int getCodigoPrecio() {
+		return codigoPrecio;
+	}
 
-		public String getNombre() {
-			return nombre;
-		}
+	public void setCodigoPrecio(int codigoPrecio) {
+		this.codigoPrecio = codigoPrecio;
+	}
 
-		public void setNombre(String nombre) {
-			this.nombre = nombre;
-		}
+	public void persist() {
+		Registrar.add("Movies", this);
+	}
+
+	public static Pelicula get(String name) {
+		return (Pelicula) Registrar.get("Peliculas", name);
+	}
+	// Yo manejaría lo que son gets y sets en una misma sección del código
+	// para evitar confusiones dentro de este
+	/*
+	 * public String getNombre() { return nombre; }
+	 * 
+	 * public void setNombre(String nombre) { this.nombre = nombre; }
+	 */
 }
