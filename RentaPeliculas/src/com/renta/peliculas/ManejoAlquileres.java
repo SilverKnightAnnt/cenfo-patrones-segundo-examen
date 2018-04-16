ba3bfba7449f6d3cf5b43c78917547c94366ed27
Ángel Marín <35677091+SilverKnightAnnt@users.noreply.github.com>
2018-04-15 19:56:52 -0600

    Eliminación de la dependencia del cliente con alquileres
    Se elimina la dependencia del cliente de manejar todo lo referente a sus alquileres.

---
+++ RentaPeliculas/src/com/renta/peliculas/ManejoAlquileres.java
@@ -0,0 +1,64 @@
+package com.renta.peliculas;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/*Esta clase se va a encargar de eliminar la dependencia que tenía el 
+Cliente de manejar sus propios alquileres y de imprimirlos, ahora ManejoAlquileres 
+tiene el método addRental() y statement()*/
+
+public class ManejoAlquileres {
+
+	private ArrayList<Alquiler> alquileres = new ArrayList<>();
+
+	public void addRental(Alquiler arg) {
+		alquileres.add(arg);
+	}
+
+	//statement ahora recibe el cliente para saber a quién asignarle los alquileres.
+	public String statement(Cliente cliente) {
+		cliente.setAlquileres(alquileres);
+		double montoTotal = 0;
+		int puntosAlquilerFrecuente = 0;
+		Iterator<Alquiler> iterator = alquileres.iterator();
+		String result = "Alquileres de " + cliente.getNombre() + ":\n";
+		while (iterator.hasNext()) {
+			double montoAlquiler = 0;
+			Alquiler alquiler = iterator.next();
+
+			// determine amounts for each line
+			switch (alquiler.getDisco().getPelicula().getCodigoPrecio()) {
+			case Pelicula.NORMAL:
+				montoAlquiler += 2;
+				if (alquiler.getDiasAlquilado() > 2)
+					montoAlquiler += (alquiler.getDiasAlquilado() - 2) * 1.5;
+				break;
+			case Pelicula.ESTRENO:
+				montoAlquiler += alquiler.getDiasAlquilado() * 3;
+				break;
+			case Pelicula.INFANTIL:
+				montoAlquiler += 1.5;
+				if (alquiler.getDiasAlquilado() > 3)
+					montoAlquiler += (alquiler.getDiasAlquilado() - 3) * 1.5;
+				break;
+
+			}
+			montoTotal += montoAlquiler;
+
+			puntosAlquilerFrecuente++;
+			// agregar bono por alquiler de pelicula "estreno"
+			if ((alquiler.getDisco().getPelicula().getCodigoPrecio() == Pelicula.ESTRENO)
+					&& alquiler.getDiasAlquilado() > 1)
+				puntosAlquilerFrecuente++;
+
+			// mostrar datos
+			result += "\t" + alquiler.getDisco().getPelicula().getNombre() + "\t" + String.valueOf(montoAlquiler)
+					+ "\n";
+
+		}
+		// fin del reporte
+		result += "Monto total:  " + String.valueOf(montoTotal) + "\n";
+		result += "Gano " + String.valueOf(puntosAlquilerFrecuente) + " puntos por alquiler frecuente";
+		return result;
+	}
+}
--- RentaPeliculas/src/com/renta/peliculas/tests/UITest.java
+++ RentaPeliculas/src/com/renta/peliculas/tests/UITest.java
@@ -5,36 +5,37 @@ import org.junit.Test;
 import com.renta.peliculas.Alquiler;
 import com.renta.peliculas.Cliente;
 import com.renta.peliculas.Disco;
+import com.renta.peliculas.ManejoAlquileres;
 import com.renta.peliculas.Pelicula;
 
 public class UITest {
 
 	@Test
 	public void realizarAlquiler() {
+		ManejoAlquileres ma = new ManejoAlquileres();
 		Pelicula pelicula = new Pelicula("Harry Potter", 2);
 		Disco disco = new Disco("E-231", pelicula);
 		Alquiler alquiler = new Alquiler(disco,7);
 		Cliente cliente = new Cliente("Danilo");		
 		
-		cliente.addRental(alquiler);
+		ma.addRental(alquiler);		
 		
-		cliente.statement();
+		ma.statement(cliente);
 	}
-	
 	@Test
-	public void realizarAlquilerDeDosPeliculas() {
+	public void realizarAlquilerDeDosPeliculas() {		
+		ManejoAlquileres ma = new ManejoAlquileres();
 		Pelicula pelicula = new Pelicula("Harry Potter", 2);
 		Disco disco = new Disco("E-231", pelicula);
 		Alquiler alquiler = new Alquiler(disco,7);
 		Cliente cliente = new Cliente("Danilo");
+		ma.addRental(alquiler);
 		Pelicula pelicula2 = new Pelicula("El señor de los anillos", 1);
 		Disco disco2 = new Disco("E-450", pelicula2);
 		Alquiler alquiler2 = new Alquiler(disco2,4);
+		ma.addRental(alquiler2);		
 		
-		cliente.addRental(alquiler);		
-		cliente.addRental(alquiler2);
-		
-		cliente.statement();
+		System.out.println(ma.statement(cliente));
 	}
 	
 	
